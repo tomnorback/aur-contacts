334a072c5d464607dc27d5c0d3422c978d170bce
Tom Norback <tomnorback@gmail.com>
2016-07-15 15:05:26 -0500

    start of dark theme, remove nav menu, add details to list in prep to remove separate details page

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
 export class App {
   configureRouter(config, router) {
-    config.title = 'Aurelia';
+    config.title = 'Contacts';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
-      { route: 'contacts',		name: 'contacts',	  moduleId: 'contacts',		nav: true, title: 'Contacts' },
+      { route: ['', 'contacts'],name: 'contacts',	  moduleId: 'contacts',		nav: true, title: 'Contacts' },
       { route: 'contactdetail', name: 'contactdetail',moduleId: 'contactdetail',nav: true, title: 'Details' },
       { route: 'contactedit',	name: "contactedit",  moduleId: 'contactedit',  nav: true, title: 'Contact Edit' }
     ]);
